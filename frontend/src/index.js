efc1ebd085308fb6da97780c4bc58f34818fc74f
Denny <denny1011133@gmail.com>
2022-08-10 13:39:01 +0800

    feat/implement AuthContext

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { RecordsContextProvider } from './context/RecordsContext';
+import { AuthContextProvider } from './context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RecordsContextProvider>
-      <App />
-    </RecordsContextProvider>
+    <AuthContextProvider>
+      <RecordsContextProvider>
+        <App />
+      </RecordsContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
